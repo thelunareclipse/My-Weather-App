d3be39d4f642a3eaa812bb3ff3e3e53943162b97
Luna <69701846+thelunareclipse@users.noreply.github.com>
2023-02-26 22:41:25 -0500

    Added in a newDate for forecast and formatted data

--- index.js
+++ index.js
@@ -39,24 +39,40 @@ document.getElementById(
   "date"
 ).innerHTML = `${day}, ${month} ${date}, ${year} ${hour}:${minute}`;
 
+function forecastDate(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
+
   let forecastHTML = `<div class="row">`;
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
         <div class="col-2">
-          <span>${day}</span>
+          <span>${forecastDate(forecastDay.dt)}</span>
           <img
-            src="http://openweathermap.org/img/wn/50d@2x.png"
+            src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png"
             alt=""
             width="38"
           />
-          <p>18° 12°</p>
+          <span>${Math.round(forecastDay.temp.max)}° </span><span>${Math.round(
+          forecastDay.temp.min
+        )}°</span>
         </div>`;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
